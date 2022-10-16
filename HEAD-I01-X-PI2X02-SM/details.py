1ce0f78fb158fa4019d93ea7c0446c0cb34b1302
Oom Lout <aaron@oomlout.com>
2022-10-16 21:16:36 +0100

    Added HEAD-I01-X-PI2*-SM

---
+++ HEAD-I01-X-PI2X02-SM/details.py
@@ -0,0 +1,31 @@
+
+import OOMP 
+import OOMPtags 
+
+######  Auto translated oomp file
+
+newPart = OOMP.oompItem()
+
+oType = "HEAD"
+oSize = "I01"
+oColor = "X"
+oDesc = "PI2X02"
+oIndex = "SM"
+hexId = "H2X2SM"
+
+newPart.addTag('oompType',oType)
+newPart.addTag('oompSize',oSize)
+newPart.addTag('oompColor',oColor)
+newPart.addTag('oompDesc',oDesc)
+newPart.addTag('oompIndex',oIndex)
+oompId = oType + "-" + oSize + "-" + oColor + "-" + oDesc + "-" + oIndex 
+
+
+
+######  Common
+newPart.addTag('hexID',hexId)
+
+######  Housekeeping
+newPart = OOMPtags.addTags(newPart,oompId)
+#newPart = OOMPtags.addTags(newPart,oompId),pitch = pitch)
+OOMP.parts.append(newPart)
